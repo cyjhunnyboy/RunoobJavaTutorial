8075dc5c8f51ed3b0f71f76456a60979d336faeb
cyjhunnyboy <cyjhunnyboy@sina.com>
2022-02-24 17:09:38 +0800

    Runoob Java Tutorial

---
+++ src/com/runoob/java/_01_tutorial/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.runoob.java._01_tutorial;
+
+/**
+ * 第一个Java程序
+ *
+ * @author chenyongjun
+ * @since 2022-02-24
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        /*
+            注意：String args[]与String[] args都可以执行，
+            但推荐使用String[] args，这样可以避免歧义与误读。
+         */
+        System.out.println("Hello World!");
+    }
+}
